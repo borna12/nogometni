cd270a110cb4928f73f9fe6fbdc8f862fafd3a28
josip <jomihx@gmail.com>
2022-07-20 09:48:30 +0200

    povijest

---
+++ povijest-prvenstava/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GogMcSLNzWoi4TygOsutAjM298xXKnZJ5IPoFy57UD8/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBxrS0lltFC6BqH5oOOIqiRrY_bWqnAmCM';
